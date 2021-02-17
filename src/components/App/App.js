f72ffb62ea1975d25c7f96e1dfcc6adeac004f95
Nicholas Simonelli <nrsimonelli@gmail.com>
2021-02-17 16:51:40 -0600

    defined color and text classes and created landing page examples for dark and light mode

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,23 +25,71 @@ const App = () => {
     <div className="App fade-in">
       <Nav />
      
-      <div>hello world</div>
       <div className="Content">
-        <div className="square">
+        <div className='test'>hello world</div>      
+      </div>
+      <div className="Content">
+        <div className="square color-red-5">
           1
         </div>
-        <div className="square">
+        <div className="square color-volcano-5">
           2
         </div>
-        <div className="square">
+        <div className="square color-orange-5">
           3
         </div>
-        <div className="square">
+        <div className="square color-gold-5">
           4
         </div>
-      </div>
-      <div>
-      <Switch  checked={isDarkMode}  onChange={toggleTheme} />
+        <div className="square color-yellow-5">
+          5
+        </div>
+        <div className="square color-lime-5">
+          6
+        </div>
+        <div className="square color-green-5">
+          7
+        </div>
+        <div className="square color-cyan-5">
+          8
+        </div>
+        <div className="square color-blue-5">
+          9
+        </div>
+        <div className="square color-geekblue-5">
+          10
+        </div>
+        <div className="square color-purple-5">
+          11
+        </div>
+        <div className="square color-magenta-5">
+          12
+        </div>
+        </div>
+        <div className='Content'>
+        <div className="test link-blue">
+          Link
+        </div>
+        <div className="test link-gold">
+          Link
+        </div>
+        <div className="test link-green">
+          Link
+        </div>
+        <div className="test link-red">
+          Link
+        </div>
+        </div>
+        
+
+      
+      <div className="Content">
+        <div className="test">
+        <Switch  checked={isDarkMode}  onChange={toggleTheme} />
+
+        </div>
+       
+      
 
       </div>
       
