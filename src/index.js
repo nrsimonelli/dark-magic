62d5512a820611a22bac048ab21c0f0c107ed109
Nick <nrsimonelli@gmail.com>
2021-02-16 19:36:20 -0600

    imported theme switcher and defined default theme in index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+import "./index.css";
+
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+
+const themes = {
+  dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
+  light: `${process.env.PUBLIC_URL}/light-theme.css`,
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeSwitcherProvider themeMap={themes} defaultTheme="light" >
+      <App />
+    </ThemeSwitcherProvider>
   </React.StrictMode>,
   document.getElementById('react-root')
 );
