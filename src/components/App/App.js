28909d1d2c4b529c14336878c7bb190e306642ef
Nick <nrsimonelli@gmail.com>
2021-02-17 20:27:54 -0600

    added a slider for color values

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import { React, useState } from 'react';
 import './App.css';
 import { useThemeSwitcher } from "react-css-theme-switcher";
-import { Switch } from 'antd';
+import { Switch, Slider } from 'antd';
 
 import Nav from '../Nav/Nav';
 
 
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState();
+  const [colorValue, setColorValue] = useState(5);
   const { switcher, status, themes } = useThemeSwitcher();
 
   const toggleTheme = (isChecked) => {
@@ -15,9 +16,30 @@ const App = () => {
     switcher({ theme: isChecked ? themes.dark : themes.light });
   };
 
+  const handleSlider = (value) => {
+    setColorValue(value);
+    console.log('color value set to', value);
+    console.log('colorValue state', colorValue);
+  }
+
   if (status === "loading") {
     return null;
   }
+
+  const colorLib = [
+    {color: 'red'},
+    {color: 'volcano'},
+    {color: 'orange'},
+    {color: 'gold'},
+    {color: 'yellow'},
+    {color: 'lime'},
+    {color: 'green'},
+    {color: 'cyan'},
+    {color: 'blue'},
+    {color: 'geekblue'},
+    {color: 'purple'},
+    {color: 'magenta'},
+  ];
 
 
 
@@ -26,44 +48,49 @@ const App = () => {
       <Nav />
      
       <div className="Content ev-2">
-        <div className='test text-primary'>hello world</div>      
+        <div className='test text-primary'>color level</div>
+        
+        <div className='test-2'>
+        <Slider min={1} max={10} onChange={handleSlider} value={colorValue} style={{ width: 400 }} />
+        </div>
+        
       </div>
       <div className="Content ev-2">
         <div className="square color-red-5">
-          1
+          red
         </div>
         <div className="square color-volcano-5">
-          2
+          volcano
         </div>
         <div className="square color-orange-5">
-          3
+          orange
         </div>
         <div className="square color-gold-5">
-          4
+          gold
         </div>
         <div className="square color-yellow-5">
-          5
+          yellow
         </div>
         <div className="square color-lime-5">
-          6
+          lime
         </div>
         <div className="square color-green-5">
-          7
+          green
         </div>
         <div className="square color-cyan-5">
-          8
+          cyan
         </div>
         <div className="square color-blue-5">
-          9
+          blue
         </div>
         <div className="square color-geekblue-5">
-          10
+          geekblue
         </div>
         <div className="square color-purple-5">
-          11
+          purple
         </div>
         <div className="square color-magenta-5">
-          12
+          magenta
         </div>
         </div>
         <div className='Content ev-2'>
