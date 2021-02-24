ddbf94e3ccae91cb64aaee1b81ea28d2e2b4c787
nrsimonelli <nrsimonelli@gmail.com>
2021-02-24 17:22:27 -0600

    removed the swtich at the bottom in favor of nav icon

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import { React, useState, useCallback, useEffect } from 'react';
 import './App.css';
 import { useThemeSwitcher } from "react-css-theme-switcher";
-import { Switch, Slider } from 'antd';
+import { Slider } from 'antd';
 
 import Nav from '../Nav/Nav';
 
@@ -26,11 +26,6 @@ const App = () => {
   const [colorValue, setColorValue] = useState(5);
   
   const { switcher, status, themes } = useThemeSwitcher();
-  
-  const toggleTheme = (isChecked) => {
-    setIsDarkMode(isChecked);
-    switcher({ theme: isChecked ? themes.dark : themes.light });
-  };
 
   const handleTheme = useCallback(() => {
     setIsDarkMode((prev) => !prev);
@@ -51,10 +46,6 @@ const App = () => {
   if (status === "loading") {
     return null;
   }
-
-  const darkToggle = <div className="test">
-    <Switch checked={isDarkMode} onChange={toggleTheme} />
-  </div>;
 
   return (
     <div className="App fade-in ev-0">
@@ -99,10 +90,9 @@ const App = () => {
         </div>
       </div>
       <div className="Content ev-2">
-        <div className="test">
-          theme switch
-        </div>
-        {darkToggle}
+       <div className='txt-sm' style={{ width: '100%', textAlign: 'center', marginTop: 20 }}>
+       developed by nrsimonelli
+         </div>
       </div>   
     </div>
   );
